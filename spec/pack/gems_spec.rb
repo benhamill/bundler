e596c4458c26a391d02122e602df018d7765ee4e
Ben Hamill <git-commits@benhamill.com>
2011-02-13 17:50:41 -0600

    Failing test for branch feature.

--- spec/pack/gems_spec.rb
+++ spec/pack/gems_spec.rb
@@ -18,5 +18,36 @@ describe "bundle pack with gems" do
     it "caches the gems" do
       bundled_app("vendor/cache/rack-1.0.0.gem").should exist
     end
+
+    describe "when there are any git sources" do
+      describe "when floating on master" do
+        before :each do
+          build_git "foo"
+
+          gemfile <<-G
+            source "file://#{gem_repo1}"
+            git "#{lib_path('foo-1.0')}" do
+              gem 'foo'
+            end
+          G
+
+          bundle :pack
+        end
+
+        fit "caches the repository" do
+          bundled_app("vendor/cache/foo/").should exist
+        end
+      end
+
+      describe "when given a ref" do
+=begin TODO
+        before(:each) do
+          build_git "foo"
+          @revision = revision_for(lib_path("foo-1.0"))
+          update_git "foo"
+        end
+=end
+      end
+    end
   end
 end
